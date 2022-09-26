089bd1eac234bb70ec8fd9697db172be870199e6
Difanie <dianimartinez566@gmail.com>
2022-09-26 12:33:13 +0700

    Testing

--- home.php
+++ home.php
@@ -6,7 +6,7 @@
 
 <div class="ds_page">
 <h1><b style="font-family:monb">Selamat Datang</b> di Portal</h1>
-<h1>Harum Sentosa Baru, Guru!</h1>
+<h1>Harum HURAHURA Sentosa Baru, Guru!</h1>
 <img src="logo clear bg.png" class="logo_ds">
 </div>			
 			
